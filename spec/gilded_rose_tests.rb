af4b996b3870dbc316dc0a9a6705fa6ff9e8f962
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-12 16:44:01 +0100

    Updates texttest_fixture file paths

---
+++ spec/gilded_rose_tests.rb
@@ -0,0 +1,11 @@
+require 'gilded_rose'
+
+class TestUntitled < Test::Unit::TestCase
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "fixme"
+  end
+
+end
